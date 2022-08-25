c9235cc7d0b88cb0aa8333d80435e6fae3f424a4
韩坤（Kiki） <Kiki.han@aishu.cn>
2022-08-25 14:13:21 +0800

    删减代码

---
+++ studio/webui/src/services/createEntity/index.ts
@@ -0,0 +1,151 @@
+/**
+ * @description 创建本体页请求
+ * @author Eden
+ * @date 2020/08/10
+ */
+
+import apiService from '@/utils/axios-http';
+import { API } from '@/services/api';
+
+/**
+ * @description 获取数据源
+ */
+const getSource = async () => await apiService.axiosGet(API.getSource);
+
+/**
+ * @description 获取数据表
+ * @param {object} data 获取数据表点参数
+ */
+const getDataList = async (data: any) => await apiService.axiosGetData(API.getDataList, data);
+
+/**
+ * @description 获取其他数据源的预览数据
+ * @param {object} data
+ */
+const getOtherPreData = async (data: any) =>
+  await apiService.axiosGetData(API.getOtherPreData, { ...data, ds_id: data.id });
+
+/**
+ * @description 获取子文件
+ * @param {object} data
+ */
+const getChildrenFile = async (data: any) => await apiService.axiosGetData(API.getChildrenFile, data);
+
+/**
+ * @description 预测本体生成知识网络数据
+ * @param {object} data
+ */
+const getFileGraphData = async (data: any) => await apiService.axiosPost(API.getFileGraphData, data);
+
+/**
+ * @description 获取所有本体
+ */
+const getAllNoumenon = async () =>
+  await apiService.axiosGetData(API.getAllNoumenon, { page: -1, size: 10, order: 'descend' });
+
+/**
+ * @description 按本体名获取内容
+ */
+const getAllNoumenonData = async (name: string) => await apiService.axiosGetData(API.getAllNoumenonData, { name });
+
+/**
+ * @description 新增本体
+ */
+const addEntity = async (data: any) => await apiService.axiosPost(API.addEntity, data);
+
+/**
+ * @description 获取模型
+ */
+const fetchModelList = async () => await apiService.axiosGet(API.fetchModelList);
+
+/**
+ * 根据名字获取本体信息
+ */
+const getEntityInfo = async (id: any) => await apiService.axiosGet(`${API.getEntityInfo}${id}`);
+
+/**
+ * @description 复制本体
+ */
+const copyGetEntityInfo = async (id: any, data: any) =>
+  await apiService.axiosPost(`${API.copyGetEntityInfo}${id}`, data);
+
+/**
+ * @description 流程3中按id获取数据源
+ */
+const getFlowSource = async (data: any) => await apiService.axiosGetData(API.getFlowSource, data);
+
+/**
+ * @description 流程修改知识网络
+ */
+const changeFlowData = async (id: any, data: any) => await apiService.axiosPost(`${API.changeFlowData}${id}`, data);
+
+/**
+ * @description 获取模型预览数据
+ */
+const getModelPreview = async (model: any) => await apiService.axiosGetData(API.getModelPreview, { model });
+
+/**
+ * @description 非结构化预测本体
+ */
+const unstructuredData = async (data: any) => await apiService.axiosPost(API.unstructuredData, data);
+
+/**
+ * @description 添加结构化预测任务
+ */
+const buildTask = async (data: any) => await apiService.axiosPost(API.buildTask, data);
+
+/**
+ * @description 获取预测任务列表
+ */
+const getEntityTasks = async (data: any) => await apiService.axiosPost(API.getEntityTasks, data);
+
+/**
+ * @description 删除任务
+ */
+const deleteEntityTask = async (data: any) => await apiService.axiosPost(API.deleteEntityTask, data);
+
+/**
+ * @description 获取预测文件状态
+ */
+const getTaskFiles = async (data: any) => await apiService.axiosGetData(API.getTaskFiles, data);
+
+/**
+ * @description 删除新增任务
+ */
+const delAllEntityTask = async (data: any) =>
+  await apiService.axiosDelete(`${API.delAllEntityTask}?ontology_id=${data}`);
+
+/**
+ * @description 修改本体名称信息
+ */
+const changeEntityTent = async (data: any, id: any) => await apiService.axiosPut(`${API.changeEntityTent}${id}`, data);
+
+/**
+ * @description 保存本体信息
+ */
+const updateEntity = async (data: any, id: any) => await apiService.axiosPut(`${API.updateEntity}${id}`, data);
+
+export default {
+  getDataList,
+  getOtherPreData,
+  getChildrenFile,
+  getFileGraphData,
+  getAllNoumenon,
+  addEntity,
+  fetchModelList,
+  getEntityInfo,
+  getFlowSource,
+  changeFlowData,
+  getAllNoumenonData,
+  getModelPreview,
+  unstructuredData,
+  buildTask,
+  getEntityTasks,
+  deleteEntityTask,
+  getTaskFiles,
+  copyGetEntityInfo,
+  delAllEntityTask,
+  changeEntityTent,
+  updateEntity,
+  getSource
+};
