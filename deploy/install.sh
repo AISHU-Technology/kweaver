4ad87bc7006f1c67674954aa0026ab403cf935fb
huang.liangliang <huang.liangliang@aishu.cn>
2022-09-07 09:27:38 +0800

    deploy

--- deploy/install.sh
+++ deploy/install.sh
@@ -1,54 +1,114 @@
 #!/bin/bash
 
-clear
-chmod 777 -R ./
-
 HOST_EXP="^(([1-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([1-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$"
 PORT_EXP="^[1-9]$|(^[1-9][0-9]$)|(^[1-9][0-9][0-9]$)|(^[1-9][0-9][0-9][0-9]$)|(^[1-5][0-9][0-9][0-9][0-9]$)|(^[6][0-5][0-5][0-3][0-5]$)"
 EMPTY_EXP="^[\s\S]*.*[^\ ][\s\S]*$"
 CONFIGDIR=./kwconfig
 CONFIGFILE=./kwconfig/kwconfig.yaml
 CONFIGBAK=./kwconfig/kwconfig.yaml.bak
+SERVICENAME="aishu-kweaver"
 
-# check Docker container include kw-studio|kw-builder|kw-engine|kw-nginx|kw-mongodb|kw-mariadb|kw-redis
-CONTAINER_NUM=$(docker ps -a | grep -E 'kw-studio|kw-builder|kw-engine|kw-nginx|kw-mongodb|kw-mariadb|kw-redis|kw-algserver' | wc -l)
-
-if [[ $CONTAINER_NUM > 0 ]]; then
-  PS3="You have already run kweaver docker images, you can remove them by above ways: "
-  select option in "auto" "manual"
-  do
-    case $option in
-    "auto")
-      docker ps -a | grep kw | awk '{print $1}' | xargs docker rm
-      break;;
-    "manual")
-      echo "Please remove kweaver docker container manually, and then run this install script."
-      exit 0
-      ;;
-    *)
-      echo "Incorrect input, please reselect.";;
-    esac
-  done
-fi
-
-function checkparam() {
-  dstatus=0
-    while [ $dstatus = 0 ]
-    do
-      if [[ $3 ]] ; then
-        read -s -p "$1" input
-      else
-        read -p "$1" input
-      fi
-
-      if [[ $input =~ $2 ]] ;then
-        tempinput=$input
-        dstatus+=1
-      else
-        echo "Incorrect input, please re-enter."
-      fi
-    done
+function checkDockerComposeCli() {
+  if [ ! -x "$(command -v docker-compose)" ]; then
+    DOCKERCOMPOSE="docker compose"
+  else
+    DOCKERCOMPOSE="docker-compose"
+  fi
 }
+
+function checkDockerComposeStatus() {
+  var=`$DOCKERCOMPOSE ls | grep $SERVICENAME | wc -l`
+  if [[  $var > 0 ]]; then
+    PS3="You have a running kweaver docker compose, you can choose above choices: "
+    select moption in "only stop service" "stop service and romove docker contain" "exit"
+    do
+      case $moption in
+      "only stop service")
+        $DOCKERCOMPOSE -p $SERVICENAME stop 
+        break;;
+      "stop service and romove docker contain")
+        $DOCKERCOMPOSE -p $SERVICENAME down
+        break;;
+      "exit")
+        exit 0;;
+      *)
+        echo "Incorrect input, please reselect.";;
+      esac
+    done
+  fi
+}
+
+function checkDockerComposeService() {
+  var=`$DOCKERCOMPOSE -p $SERVICENAME ps | wc -l`
+  if [[ $var > 1 ]]; then
+    PS3="You have a stoped kweaver docker compose, you can choose above choices: "
+    select moption in "start service" "romove docker contain"
+    do
+      case $moption in
+      "start service")
+        $DOCKERCOMPOSE -p $SERVICENAME start
+        exit 0;;
+      "romove docker contain")
+        $DOCKERCOMPOSE -p $SERVICENAME rm
+        break;;
+      *)
+        echo "Incorrect input, please reselect.";;
+      esac
+    done
+  fi
+}
+
+function checkServiceName() {
+  var=`docker ps -a | grep -E 'kw-studio|kw-builder|kw-engine|kw-nginx|kw-mongodb|kw-mariadb|kw-redis|kw-algserver' | wc -l`
+  if [[ $var > 0 ]]; then
+    PS3="Container names has been used, you can remove them by above ways: "
+    select option in "auto" "manual"
+    do
+      case $option in
+      "auto")
+        docker ps -a | grep kw | awk '{print $1}' | xargs docker rm
+        break;;
+      "manual")
+        read -p "Remove containers munually, press anykey to continue." conti
+        echo -e "\n"
+        case $conti in
+        *)
+          break
+          ;;
+        esac
+        ;;
+      *)
+        echo "Incorrect input, please reselect.";;
+      esac
+    done
+  fi
+}
+
+function checkParameters() {
+  dstatus=0
+  while [ $dstatus = 0 ]
+  do
+    if [[ $3 ]] ; then
+      read -s -p "$1" input
+    else
+      read -p "$1" input
+    fi
+
+    if [[ $input =~ $2 ]] ;then
+      tempinput=$input
+      dstatus+=1
+    else
+      echo "Incorrect input, please re-enter."
+    fi
+  done
+}
+
+clear
+chmod 777 -R ./
+checkDockerComposeCli
+checkDockerComposeStatus
+checkDockerComposeService
+checkServiceName
 
 read -p "Do you want to use the project's default database [Y/N]?  " answer
 echo -e "\n"
@@ -63,20 +123,35 @@ case $answer in
     fi
   }
 
-  function touchyaml() {
+  function operateWithYaml() {
     if [[ ! -d "$CONFIGDIR" ]]; then
       mkdir $CONFIGDIR
     fi
     if [[ ! -f "$CONFIGFILE" ]]; then
+      OPERATETYPE="newtype"
       touch $CONFIGFILE
     else
       if [[ -f "$CONFIGBAK" ]]; then
         rm -rf $CONFIGBAK
       fi
-      mv $CONFIGFILE $CONFIGBAK
-      touch $CONFIGFILE
-    fi
-    chmod 777 $CONFIGFILE
+      PS3="Find datebase config file, you can do above things: "
+      select option in "edit config file" "create new config file"
+      do
+        case $option in
+        "edit config file")
+          OPERATETYPE="edittype"
+          break;;
+        "create new config file")
+          OPERATETYPE="newtype"
+          mv $CONFIGFILE $CONFIGBAK
+          touch $CONFIGFILE
+          chmod 777 $CONFIGFILE
+          break;;
+        *)
+          echo "Incorrect input, please reselect.";;
+        esac
+      done
+    fi  
   }
 
   function edityaml() {
@@ -104,16 +179,16 @@ case $answer in
 
   function editdbconfig() {
     echo "Use your own $1 database."
-    checkparam "Enter your $1 host: " "$EMPTY_EXP"
+    checkParameters "Enter your $1 host: " "$EMPTY_EXP"
     add=$tempinput
-    checkparam "Enter your $1 port: " "$PORT_EXP"
+    checkParameters "Enter your $1 port: " "$PORT_EXP"
     port=$tempinput
-    checkparam "Enter your $1 account: " "$EMPTY_EXP"
+    checkParameters "Enter your $1 account: " "$EMPTY_EXP"
     user=$tempinput
-    checkparam "Enter your $1 password: " "$EMPTY_EXP" "-s"
+    checkParameters "Enter your $1 password: " "$EMPTY_EXP" "-s"
     pwd=$tempinput
     echo -e "\r"
-    checkparam "Enter your $1 database: " "$EMPTY_EXP"
+    checkParameters "Enter your $1 database: " "$EMPTY_EXP"
     db=$tempinput
     echo -e "\n"
     # mariadb & mongodb parameters: $1:db $2:host $3:port $4:account $5:password $6:dbname
@@ -123,29 +198,29 @@ case $answer in
   function configredis() {
     case $1 in
     standalone)
-      checkparam "Enter your redis host: " "$EMPTY_EXP"
+      checkParameters "Enter your redis host: " "$EMPTY_EXP"
       add=$tempinput
-      checkparam "Enter your redis port: " "$PORT_EXP"
+      checkParameters "Enter your redis port: " "$PORT_EXP"
       port=$tempinput
-      checkparam "Enter your redis account: " ""
+      checkParameters "Enter your redis account: " ""
       user=$tempinput
-      checkparam "Enter your redis password: " "" "-s"
+      checkParameters "Enter your redis password: " "" "-s"
       pwd=$tempinput
       echo -e "\n"
       # redis standalone parameters: $1:db $2:mode $3:host $4:port $5:account $6:password $7:dbname
       edityaml redis stand-alone $add $port "$user" "$pwd"
       ;;
     sentinel)
-      checkparam "Enter sentinel mode redis master node name: " "$EMPTY_EXP"
+      checkParameters "Enter sentinel mode redis master node name: " "$EMPTY_EXP"
       master=$tempinput
-      checkparam "Enter sentinel mode redis sentinel account: " ""
+      checkParameters "Enter sentinel mode redis sentinel account: " ""
       suser=$tempinput
-      checkparam "Enter sentinel mode redis sentinel password: " "" "-s"
+      checkParameters "Enter sentinel mode redis sentinel password: " "" "-s"
       spwd=$tempinput
       echo -e "\r"
-      checkparam "Enter sentinel mode redis account: " ""
+      checkParameters "Enter sentinel mode redis account: " ""
       user=$tempinput
-      checkparam "Enter sentinel mode redis password: " "" "-s"
+      checkParameters "Enter sentinel mode redis password: " "" "-s"
       pwd=$tempinput
       echo -e "\n"
       # redis sentinel parameters: $1:db $2:mode $3:master name $4:sentinel account $5:sentinel password $6:db account $7:db password
@@ -158,13 +233,13 @@ case $answer in
       else
         sname="slave_$3 node"
       fi
-      checkparam "Enter cluster mode redis $sname host: " "$EMPTY_EXP"
+      checkParameters "Enter cluster mode redis $sname host: " "$EMPTY_EXP"
       add=$tempinput
-      checkparam "Enter cluster mode redis $sname port: " "$PORT_EXP"
+      checkParameters "Enter cluster mode redis $sname port: " "$PORT_EXP"
       port=$tempinput
-      checkparam "Enter cluster mode redis $sname account: " ""
+      checkParameters "Enter cluster mode redis $sname account: " ""
       user=$tempinput
-      checkparam "Enter cluster mode redis $sname password: " "" "-s"
+      checkParameters "Enter cluster mode redis $sname password: " "" "-s"
       pwd=$tempinput
       echo -e "\n"
       # redis cluster parameters: $1:db $2:mode $3:host $4:port $5:account $6:password $8:-node type
@@ -175,28 +250,28 @@ case $answer in
 
   function sentinelnodeconfig() {
     dstatus=0
-      while [ $dstatus = 0 ]
-      do
-        read -p "Enter sentinel mode redis sentinel node number: " sentinelnu
-        if [ $sentinelnu -ge 1 ] 2>/dev/null ;then
-        for ((i=1; i<=$sentinelnu; i++))
-          do
-            sentinelarr $i
-            if [ $i -eq $sentinelnu ]; then
-              configredis sentinel
-              dstatus+=1
-            fi
-          done
-        else
-          echo "Sentinel nodes must be at least 1, please re-enter."
-        fi
-      done
+    while [ $dstatus = 0 ]
+    do
+      read -p "Enter sentinel mode redis sentinel node number: " sentinelnu
+      if [ $sentinelnu -ge 1 ] 2>/dev/null ;then
+      for ((i=1; i<=$sentinelnu; i++))
+        do
+          sentinelarr $i
+          if [ $i -eq $sentinelnu ]; then
+            configredis sentinel
+            dstatus+=1
+          fi
+        done
+      else
+        echo "Sentinel nodes must be at least 1, please re-enter."
+      fi
+    done
   }
 
   function sentinelarr() {
-    checkparam "Enter sentinel mode redis sentinel_$1 host: " "$EMPTY_EXP"
+    checkParameters "Enter sentinel mode redis sentinel_$1 host: " "$EMPTY_EXP"
     add=$tempinput
-    checkparam "Enter sentinel mode redis sentinel_$1 port: " "$PORT_EXP"
+    checkParameters "Enter sentinel mode redis sentinel_$1 port: " "$PORT_EXP"
     port=$tempinput
     echo -e "\n"
     index=`expr $1 - 1`
@@ -231,32 +306,41 @@ case $answer in
 
   installyq
 
-  touchyaml
+  operateWithYaml
 
-  editdbconfig mariadb
-
-  editdbconfig mongodb
-
-  PS3="Choose your reids deployment mode: "
-  select option in "stand-alone mode" "sentinel mode"
-  # select option in "stand-alone mode" "sentinel mode" "cluster mode"
-  do
-    case $option in
-    "stand-alone mode")
-      configredis standalone
-      break;;
-    "sentinel mode")
-      sentineladd=()
-      sentinelport=()
-      sentinelnodeconfig
-      break;;
-    # "cluster mode")
-    #   redis_cluster
-    #   break;;
-    *)
-      echo "Incorrect input, please reselect.";;
+  if [ $OPERATETYPE = "newtype" ]; then
+    editdbconfig mariadb
+    editdbconfig mongodb
+    PS3="Choose your reids deployment mode: "
+    select option in "stand-alone mode" "sentinel mode"
+    # select option in "stand-alone mode" "sentinel mode" "cluster mode"
+    do
+      case $option in
+      "stand-alone mode")
+        configredis standalone
+        break;;
+      "sentinel mode")
+        sentineladd=()
+        sentinelport=()
+        sentinelnodeconfig
+        break;;
+      # "cluster mode")
+      #   redis_cluster
+      #   break;;
+      *)
+        echo "Incorrect input, please reselect.";;
+      esac
+    done
+  else
+    read -p "Edit the database config file manually, press anykey begin to edit." conti
+    echo -e "\n"
+    case $conti in
+    (*)
+      vi $CONFIGFILE
+      break
+      ;;
     esac
-  done
+  fi
   ;;
 (*)
   echo "You have selected an unknown option, will use the project's default database.";;
@@ -266,14 +350,12 @@ esac
 
 function dockercomposeinit() {
   # edit nginx config
-  checkparam "Enter the host that you want to deploy kweaver: " "$EMPTY_EXP"
+  checkParameters "Enter the host that you want to deploy kweaver: " "$EMPTY_EXP"
   sed -i '/.*server_name*./c\server_name  '"$tempinput"';' ./nginx/nginx.conf
-
-
-  docker compose -p "aishu-kweaver" -f $1 up
+  $DOCKERCOMPOSE -p $SERVICENAME -f $1 up -d
 }
 
-checkeditports() {
+function checkeditports() {
   pIDa=`lsof -i :$1 | grep -v "PID" | awk '{print $2}'`
   if [[ "$pIDa" != "" ]];then
     echo "port $1 has been used, you should edit ./docker-compose-$2.yaml"
@@ -283,7 +365,7 @@ checkeditports() {
   fi
 }
 
-basicoperation() {
+function basicoperation() {
   # check ports kw-nginx:80 kw-studio:6800 kw-builder:6475 kw-algserver:8080 kw-engine:6474
   checkeditports 80 "basic" "nginx"
   checkeditports 6800 "basic" "nginx"
@@ -293,7 +375,7 @@ basicoperation() {
   dockercomposeinit ./docker-compose-basic.yaml
 }
 
-advanceoperation() {
+function advanceoperation() {
   # check ports kw-nginx:80 kw-mariadb:3307 kw-redis:6378 kw-mongodb:27018 kw-studio:6800 kw-builder:6475 kw-algserver:8080 kw-engine:6474
   checkeditports 80 "advance" "nginx"
   checkeditports 3307 "advance" ""
@@ -316,13 +398,15 @@ if [[ $answer = "N" ]]||[[ $answer = "n" ]];then
     basicoperation
     ;;
   (N | n)
-    read -p "Edit the database config file munually, press anykey to continue." conti
+    read -p "Edit the database config file manually, press anykey begin to edit." conti
     echo -e "\n"
     case $conti in
     (*)
+      vi $CONFIGFILE
       basicoperation
+      break
       ;;
-    esac
+    esac      
     ;;
   (*)
     echo "You have selected an unknown option, will continue."
