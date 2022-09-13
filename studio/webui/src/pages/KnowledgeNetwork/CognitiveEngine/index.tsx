a7060016de5a84aff2971e1843d913f45be84ee4
韩坤（Kiki） <Kiki.han@aishu.cn>
2022-09-13 16:13:45 +0800

    样式

--- studio/webui/src/pages/KnowledgeNetwork/CognitiveEngine/index.tsx
+++ studio/webui/src/pages/KnowledgeNetwork/CognitiveEngine/index.tsx
@@ -2,6 +2,8 @@
  * 认知引擎
  */
 import React, { memo } from 'react';
+
+import Format from '@/components/Format';
 import CognitiveSearch from './CognitiveSearch';
 
 import './style.less';
@@ -15,6 +17,9 @@ const CognitiveEngine: React.FC<CognitiveEngineProps> = props => {
 
   return (
     <div className="kg-cognitive-engine">
+      <div className="ad-border-b ad-bg-white ad-pl-6 ad-pr-6">
+        <Format.Title className="ad-mt-5 ad-mb-5">知识网络搜索</Format.Title>
+      </div>
       <CognitiveSearch kgData={kgData} />
     </div>
   );
