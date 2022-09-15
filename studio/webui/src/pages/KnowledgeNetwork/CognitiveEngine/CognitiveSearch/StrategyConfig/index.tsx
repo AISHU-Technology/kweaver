3a27919a628fa3d5d76a9986a6fb2d77089de762
韩坤（Kiki） <Kiki.han@aishu.cn>
2022-09-15 15:21:45 +0800

    样式调整

--- studio/webui/src/pages/KnowledgeNetwork/CognitiveEngine/CognitiveSearch/StrategyConfig/index.tsx
+++ studio/webui/src/pages/KnowledgeNetwork/CognitiveEngine/CognitiveSearch/StrategyConfig/index.tsx
@@ -533,11 +533,7 @@ const StrategyConfig: React.ForwardRefRenderFunction<unknown, StrategyConfigProp
             <div className="config-row">
               <p className="row-title">
                 {intl.get('searchConfig.searchScope')}
-                <Tooltip
-                  getPopupContainer={triggerNode => triggerNode.parentElement!}
-                  placement="right"
-                  title={intl.get('searchConfig.scopeTip')}
-                >
+                <Tooltip placement="right" title={intl.get('searchConfig.scopeTip')}>
                   <QuestionCircleOutlined className="q-icon" />
                 </Tooltip>
               </p>
@@ -588,11 +584,7 @@ const StrategyConfig: React.ForwardRefRenderFunction<unknown, StrategyConfigProp
             <div className="config-row">
               <p className="row-title" style={{ marginTop: 24 }}>
                 {intl.get('searchConfig.searchRes')}
-                <Tooltip
-                  getPopupContainer={triggerNode => triggerNode!}
-                  placement="right"
-                  title={intl.get('searchConfig.resTip')}
-                >
+                <Tooltip placement="right" title={intl.get('searchConfig.resTip')}>
                   <QuestionCircleOutlined className="q-icon" />
                 </Tooltip>
               </p>
