381e9f32c0d9a9a4fe7f0afdaeb7a5a11336b824
Haiyan.dong <Haiyan.dong@aishu.cn>
2022-09-15 14:37:50 +0800

    proxy

--- studio/webui/src/setupProxy.js
+++ studio/webui/src/setupProxy.js
@@ -11,12 +11,13 @@ module.exports = function (app) {
       // target: 'https://10.4.106.255',
       // target: 'https://10.4.129.24',
       // target: 'https://10.4.69.176',
-      // target: 'https://10.2.174.230',
+      // target: 'http://10.2.174.230',
       // target: 'https://10.240.0.42',
       // target: 'https://10.2.235.242',
       // target: 'https://10.2.196.57',
       // target: 'https://10.4.71.138',
-      target: 'http://10.2.174.230:6475',
+      // target: 'http://10.2.174.230:6475',
+      target: 'http://10.4.69.51',
       changeOrigin: true,
       secure: false
     })
@@ -30,12 +31,13 @@ module.exports = function (app) {
       // target: 'https://10.4.106.255',
       // target: 'https://10.4.129.24',
       // target: 'https://10.4.69.176',
-      // target: 'https://10.2.174.230',
+      // target: 'http://10.2.174.230',
       // target: 'https://10.240.0.42',
       // target: 'https://10.2.235.242',
       // target: 'https://10.2.196.57',
       // target: 'https://10.4.71.138',
-      target: 'http://10.2.174.230:6800',
+      // target: 'http://10.2.174.230:6800',
+      target: 'http://10.4.69.51',
       changeOrigin: true,
       secure: false
     })
@@ -43,18 +45,18 @@ module.exports = function (app) {
   app.use(
     createProxyMiddleware('/api/engine', {
       // target: 'https://10.4.68.144',
-      // target: 'https://10.4.69.53',
+      target: 'http://10.4.69.51',
       // target: 'https://10.2.174.228',
       // target: 'https://10.4.107.186',
       // target: 'https://10.4.106.255',
       // target: 'https://10.4.129.24',
       // target: 'https://10.4.69.176',
-      // target: 'https://10.2.174.230',
+      // target: 'http://10.2.174.230',
       // target: 'https://10.240.0.42',
       // target: 'https://10.2.235.242',
       // target: 'https://10.2.196.57',
       // target: 'https://10.4.71.138',
-      target: 'http://10.2.174.230:6474',
+      // target: 'http://10.2.174.230:6474',
       changeOrigin: true,
       secure: false
     })
