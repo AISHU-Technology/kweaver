da5897860837352d0c7b5800ca6cf33ac31b5875
Brooks.he <Brooks.he@aishu,cn>
2022-10-19 11:18:22 +0800

    加回来expande，searche，searchv

--- engine/graph-engine/services/explore.go
+++ engine/graph-engine/services/explore.go
@@ -2,91 +2,92 @@ package services
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
 	"graph-engine/controllers"
 	"graph-engine/utils"
 )
 
-//type ReqSearchVArgs struct {
-//	ID               string
-//	Class            string                  `json:"class" binding:"required"`
-//	Q                string                  `json:"query" binding:"omitempty"`
-//	Page             int32                   `json:"page" binding:"required,gt=0"`
-//	Size             int32                   `json:"size" binding:"required,gt=0"`
-//	QueryAll         bool                    `json:"query_all"`
-//	SearchFilterArgs *utils.SearchFilterArgs `json:"filter" binding:"required"`
-//}
-//
-//// searchV
-//func KGSearchVHandler(c *gin.Context) {
-//	var body ReqSearchVArgs
-//
-//	body.ID, _ = c.Params.Get("id")
-//
-//	err := c.ShouldBindWith(&body, binding.JSON)
-//	if err != nil {
-//		c.JSON(400, utils.ErrInfo(utils.ErrArgsErr, err))
-//		return
-//	}
-//	//fmt.Println(body)
-//
-//	status, res := controllers.KGSearchV(body.ID, body.Class, body.Q, body.Page, body.Size, body.QueryAll, body.SearchFilterArgs)
-//
-//	c.JSON(status, gin.H{
-//		"res": res,
-//	})
-//}
+type ReqSearchVArgs struct {
+	ID               string
+	Class            string                  `json:"class" binding:"required"`
+	Q                string                  `json:"query" binding:"omitempty"`
+	Page             int32                   `json:"page" binding:"required,gt=0"`
+	Size             int32                   `json:"size" binding:"required,gt=0"`
+	QueryAll         bool                    `json:"query_all"`
+	SearchFilterArgs *utils.SearchFilterArgs `json:"filter" binding:"required"`
+}
 
-//type ReqSearchEArgs struct {
-//	ID  string
-//	Rid string `form:"rid" binding:"required"`
-//}
+// searchV
+func KGSearchVHandler(c *gin.Context) {
+	var body ReqSearchVArgs
 
-// searchE
-//func KGSearchEHandler(c *gin.Context) {
-//	var body ReqSearchEArgs
-//	err := c.ShouldBind(&body)
-//	if err != nil {
-//		c.JSON(400, utils.ErrInfo(utils.ErrArgsErr, err))
-//		return
-//	}
-//
-//	body.ID, _ = c.Params.Get("id")
-//
-//	status, res := controllers.KGSearchE(body.ID, body.Rid)
-//
-//	c.JSON(status, gin.H{
-//		"res": res,
-//	})
-//}
-//
-//type ReqExpandEArgs struct {
-//	ID    string
-//	Class string `form:"class" binding:"required"`
-//	IO    string `form:"io" binding:"required"`
-//	Rid   string `form:"rid" binding:"required"`
-//	Page  int32  `form:"page" binding:"required,gt=0"`
-//	Size  int32  `form:"size" binding:"required,gt=0"`
-//}
-//
-//// expandE
-//func KGExpandEHandler(c *gin.Context) {
-//	var body ReqExpandEArgs
-//
-//	err := c.ShouldBind(&body)
-//	if err != nil {
-//		c.JSON(400, utils.ErrInfo(utils.ErrArgsErr, err))
-//		return
-//	}
-//
-//	body.ID, _ = c.Params.Get("id")
-//
-//	status, res := controllers.KGExpandE(body.ID, body.Class, body.IO, body.Rid, body.Page, body.Size)
-//
-//	c.JSON(status, gin.H{
-//		"res": res,
-//	})
-//}
-//
+	body.ID, _ = c.Params.Get("id")
+
+	err := c.ShouldBindWith(&body, binding.JSON)
+	if err != nil {
+		c.JSON(400, utils.ErrInfo(utils.ErrArgsErr, err))
+		return
+	}
+	//fmt.Println(body)
+
+	status, res := controllers.KGSearchV(body.ID, body.Class, body.Q, body.Page, body.Size, body.QueryAll, body.SearchFilterArgs)
+
+	c.JSON(status, gin.H{
+		"res": res,
+	})
+}
+
+type ReqSearchEArgs struct {
+	ID  string
+	Rid string `form:"rid" binding:"required"`
+}
+
+//searchE
+func KGSearchEHandler(c *gin.Context) {
+	var body ReqSearchEArgs
+	err := c.ShouldBind(&body)
+	if err != nil {
+		c.JSON(400, utils.ErrInfo(utils.ErrArgsErr, err))
+		return
+	}
+
+	body.ID, _ = c.Params.Get("id")
+
+	status, res := controllers.KGSearchE(body.ID, body.Rid)
+
+	c.JSON(status, gin.H{
+		"res": res,
+	})
+}
+
+type ReqExpandEArgs struct {
+	ID    string
+	Class string `form:"class" binding:"required"`
+	IO    string `form:"io" binding:"required"`
+	Rid   string `form:"rid" binding:"required"`
+	Page  int32  `form:"page" binding:"required,gt=0"`
+	Size  int32  `form:"size" binding:"required,gt=0"`
+}
+
+// expandE
+func KGExpandEHandler(c *gin.Context) {
+	var body ReqExpandEArgs
+
+	err := c.ShouldBind(&body)
+	if err != nil {
+		c.JSON(400, utils.ErrInfo(utils.ErrArgsErr, err))
+		return
+	}
+
+	body.ID, _ = c.Params.Get("id")
+
+	status, res := controllers.KGExpandE(body.ID, body.Class, body.IO, body.Rid, body.Page, body.Size)
+
+	c.JSON(status, gin.H{
+		"res": res,
+	})
+}
+
 type ReqExpandVArgs struct {
 	ID    string `form:"id" binding:"required"`
 	Class string `form:"class"`
